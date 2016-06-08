907a35d9d4416ad964f5f6e6a5ee4d196a047928
amrfarid <amr.farid222@gmail.com>
2016-06-08 15:12:46 +0000

    Modifying README to show how to use docker command

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# task1
+
+```
+ # docker run -it -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION -e KeyPair=$KeyPair  --name=coreos afarid/coreos
+```
--- launch.py
+++ launch.py
@@ -1,5 +1,18 @@
+import os
 import yaml
 import json
 import sys
 import boto3
 
+print("Creating new stack ... ")
+f = open(os.environ["Template"],'r')
+template = f.read()
+f.close()
+
+client = boto3.client('cloudformation')
+client.create_stack(
+    StackName=os.environ["StackName"],
+    TemplateBody= template,
+    Parameters=os.environ["Parameters"],
+    TimeoutInMinutes=123,
+)
